383c77572bba05392361503fd3949275e78a3aa0
maitreya29 <maitreyapatni30@gmail.com>
2021-10-05 17:32:53 +0530

    miatoll: Generic-ify
    Change-Id: I906dbbb5b16542d5c1e6a924bde875940ab980e0

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_miatoll.mk
+    $(LOCAL_DIR)/miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_miatoll-user \
-    lineage_miatoll-userdebug \
-    lineage_miatoll-eng
+    miatoll-user \
+    miatoll-userdebug \
+    miatoll-eng
