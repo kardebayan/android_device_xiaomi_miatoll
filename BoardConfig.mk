ca0baf9ce34cca5e5e0cd967b1a77235ee681cf2
Michael Bestas <mkbestas@lineageos.org>
2022-11-14 13:11:09 +0200

    miatoll: Compile the kernel without GCC
    Change-Id: I882c0d8600f88c1bb939c7ee247230ec5e70338a
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,6 +73,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-str
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
 TARGET_KERNEL_CONFIG := vendor/xiaomi/miatoll_defconfig
 TARGET_KERNEL_CLANG_VERSION := r563880c
+TARGET_KERNEL_NO_GCC := true
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
